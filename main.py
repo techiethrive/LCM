fd93fc51a019da3425a45ba96881185d92a0bb98
mujeeb <mujeeb91@hotmail.com>
2021-10-13 21:08:15 +0500

    Used python shorthand for conditional assignment.

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 def calculate_lcm(x, y):  
     # selecting the greater number  
-    if x > y:  
-        greater = x  
-    else:  
-        greater = y  
+    greater = x if x > y else y
     while(True):  
         if((greater % x == 0) and (greater % y == 0)):  
             lcm = greater  
